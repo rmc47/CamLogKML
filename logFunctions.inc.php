05888b4732f2bb4b89ddd3b49084a376c7074802
wdjc100 <git@m0zxa.co.uk>
2013-06-25 16:24:47 +0100

    Highlights last 5 QSO's in green, to assist with display of possible open paths.

---
+++ logFunctions.inc.php
@@ -0,0 +1,28 @@
+<?php
+mysql_connect('localhost', 'camlog', 'g3pyeflossie');
+$dbName = "camlog_" . mysql_real_escape_string($_GET["db"]);
+
+function getSquareColour($square) {
+	global $dbName;
+
+	$res = mysql_db_query($dbName, "SELECT COUNT(*) FROM log WHERE locator LIKE '$square%'") or die ("Error getting whether square worked" . mysql_error());
+	if ($arr = mysql_fetch_array($res)) {
+		$count = $arr[0];
+		if ($count > 0)
+			return "green";
+		else
+			return "red";
+	}
+}
+
+function getContacts() {
+	global $dbName;
+	
+	$res = mysql_db_query($dbName, "SELECT callsign, locator FROM log ORDER BY id DESC") or die ("Error getting log entries"); // List backwards so we can count off first ten fordifferent colour pin
+	$i = 0;
+	$contacts = array();
+	while ($arr = mysql_fetch_array($res)) {
+		$contacts[$arr[0]] = $arr[1];
+	}
+	return $contacts;
+}
